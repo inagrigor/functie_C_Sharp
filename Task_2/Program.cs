d085219ff9bd55397093b65e8af09b644ca7923f
Grigor_Ina <grigorina72@gmail.com>
2024-02-06 08:56:38 +0200

    add Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Вывод на экран квадратов чисел от 1 до N.
+
+// void PrintSquares(int n)
+// {
+//     int i = 1;
+//     while (i < n) 
+//     {
+//         Console.Write($"{i * i} ");
+//         i++;
+//     }
+// }
+// PrintSquares(5);
+// PrintSquares(10);
+// PrintSquares(15);
+
+
+

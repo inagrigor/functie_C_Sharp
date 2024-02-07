74e81c17f09b883b6bf15c1e0853e61c8d018480
Grigor_Ina <grigorina72@gmail.com>
2024-02-07 08:37:31 +0200

    add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,31 @@
+﻿// // 
+// void Foo(int a)
+// {
+//     Console.Write(a);
+// }
+// int value = 5;
+// Foo(value);
+
+// Обнуляем четные элименты массива
+
+// void ZeroEvenElements(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] % 2 == 0)
+//         {
+//             arr[i] = 0;
+//         }
+//     }
+// }
+// void PrintArray(int[] arr)
+// {
+//     foreach (int e in arr)
+//     {
+//         Console.Write($"{e} ");
+//     }
+// }
+
+// int[] array = { 10, 20, 30, 40, 50 };
+// ZeroEvenElements(array);
+// PrintArray(array);

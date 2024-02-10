83491830e1908039c2711e03ff56fe8d12bb7a3b
Grigor_Ina <grigorina72@gmail.com>
2024-02-10 16:20:10 +0200

    Sarcina 2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,12 +1,29 @@
-﻿double CalculateFormula(int a, int b, int c, int d)
-{
-    double numenator = a * b;
-    int denomenator = c + d;
-    double result = numenator / denomenator;
-    return result;
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// double CalculateFormula(int a, int b, int c, int d)
+// {
+//     double numenator = a * b;
+//     int denomenator = c + d;
+//     double result = numenator / denomenator;
+//     return result;
 
-}
+// }
 
-double result = CalculateFormula(1, 2, 3, 4);
+// double result = CalculateFormula(1, 2, 3, 4);
 
-Console.WriteLine(result);
+// Console.WriteLine(result);
+
+
+// void PrintSquares(int limit)
+// {
+//     int i = 1;
+//     while (i <= limit) 
+//     {
+//         Console.Write($"{i * i} ");
+//         i++;
+//     }
+// }
+
+// PrintSquares(5);
+// PrintSquares(10);
+// PrintSquares(15);
